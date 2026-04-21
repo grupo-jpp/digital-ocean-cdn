c448bca0c82130395feef33c0d11ee8d42c45029
edersign <designjppgv@gmail.com>
2026-04-21 16:14:39 -0300

    feat: add logging for connection detail setup and type changes in DigitalOcean CDN

--- client/modules/digital-ocean-cdn/src/views/connection/record/detail.js
+++ client/modules/digital-ocean-cdn/src/views/connection/record/detail.js
@@ -4,13 +4,23 @@ Espo.define(
   (Dep) => {
     return Dep.extend({
       setup() {
+        console.log(
+          "[DO-CDN] connection detail setup, type=",
+          this.model.get("type")
+        );
         Dep.prototype.setup.call(this);
+        console.log(
+          "[DO-CDN] after parent setup, detailLayout=",
+          this.detailLayout
+        );
 
         if (this.model.get("type") === "doSpaces") {
           this.applyDoSpacesLayout();
+          console.log("[DO-CDN] applied DO layout", this.detailLayout);
         }
 
         this.listenTo(this.model, "change:type", () => {
+          console.log("[DO-CDN] type changed to", this.model.get("type"));
           if (this.model.get("type") === "doSpaces") {
             this.applyDoSpacesLayout();
             this.reRender();
