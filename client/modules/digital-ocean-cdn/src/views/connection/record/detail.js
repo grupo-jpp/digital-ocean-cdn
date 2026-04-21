989b8d0604915b8f909acb5b0c6026b2f3eb3687
edersign <designjppgv@gmail.com>
2026-04-21 16:05:49 -0300

    feat: implement dynamic layout for DigitalOcean Spaces details based on connection type

--- client/modules/digital-ocean-cdn/src/views/connection/record/detail.js
+++ client/modules/digital-ocean-cdn/src/views/connection/record/detail.js
@@ -6,6 +6,22 @@ Espo.define(
       setup() {
         Dep.prototype.setup.call(this);
 
+        if (this.model.get("type") === "doSpaces") {
+          this.applyDoSpacesLayout();
+        }
+
+        this.listenTo(this.model, "change:type", () => {
+          if (this.model.get("type") === "doSpaces") {
+            this.applyDoSpacesLayout();
+            this.reRender();
+          } else if (this.model.previous("type") === "doSpaces") {
+            this.detailLayout = null;
+            this.reRender();
+          }
+        });
+      },
+
+      applyDoSpacesLayout() {
         this.detailLayout = [
           {
             label: "Details",
@@ -13,6 +29,7 @@ Espo.define(
               [{ name: "name" }, { name: "type" }],
               [{ name: "doSpacesEndpoint" }, { name: "doSpacesRegion" }],
               [{ name: "doSpacesAccessKey" }, { name: "doSpacesSecretKey" }],
+              [{ name: "doSpacesBucket" }, { name: "doSpacesCdnEndpoint" }],
             ],
           },
         ];
